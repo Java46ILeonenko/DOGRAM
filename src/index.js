ba2b552665bdcae404302f4c2c4160a7b95f557d
IgorLeonenko <leonenko.i13i@gmail.com>
2022-01-06 19:24:41 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-console.log("script launched");
-let detailsImage=document.querySelector(".details-image");
-let detailsTitle=document.querySelector(".details-title");
-let detailsNamesColon=document.querySelectorAll(".thumbnails-title")
-let anchors=document.querySelectorAll(".thumbnails-anchor");
-for(let i=0; i<anchors.length; i++){
-    anchors[i].addEventListener("click", function(event){
+console.log("Script launched");
+let detailsImage = document.querySelector(".details-image");
+let anchors = document.querySelectorAll(".thumbnails-anchor");
+let detailsTitle = document.querySelector(".details-title");
+let mainContentEl = document.querySelector(".main-content");
+let selectedItem;
+for(let i = 0; i < anchors.length; i++) {
+    anchors[i].addEventListener("click", function(event) {
         event.preventDefault();
-        setDetails(anchors[i],detailsNamesColon[i]);
-    })
+        showDetails();
+        setDetails(anchors[i]);
+    });
 }
-function setDetails(anchor,colon){
-    console.log("anchor element was pressed", anchor);
-    detailsImage.setAttribute("src", anchor.getAttribute("href"));
-    detailsTitle.textContent=colon.textContent+": "+anchor.getAttribute("data-details-title");
+function setDetails(anchor){
+    console.log("anchor was pressed", anchor);
+    let hrefValue = anchor.getAttribute("href");
+    detailsImage.setAttribute("src", hrefValue);
+    anchor.parentElement.classList.add("selected");
+    if (selectedItem && selectedItem!=anchor.parentElement){
+        selectedItem.classList.remove("selected");
+    }
+    selectedItem = anchor.parentElement;
+    let thumbnailsTitleSelector = `[href="${hrefValue}"] .thumbnails-title`;
+    let thumbnailsTitleElement = document.querySelector(thumbnailsTitleSelector);
+    detailsTitle.textContent = `${thumbnailsTitleElement.textContent}: ${anchor.getAttribute("data-details-title")}` ;
+
+}
+function showDetails() {
+    mainContentEl.classList.remove("hidden");
+}
+function hideDetails() {
+    mainContentEl.classList.add("hidden");
+    selectedItem.classList.remove("selected");
 }
